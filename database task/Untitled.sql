6a8f49a81a20c4a32d13b6c59b62f806be691fce
leta <hyeok8366@gmail.com>
2024-07-16 13:44:17 +0900

    DB 2차 과제제출

---
+++ database task/Untitled.sql
@@ -0,0 +1,11 @@
+-- CREATE DATABASE mydatabase;
+-- SHOW DATABASES;
+-- USE mydatabase;
+-- DROP DATABASE IF EXISTS mydatabase;
+
+-- USE mysql;
+-- CREATE USER 'username'@'localhost' IDENTIFIED BY 'oz-password'; 
+SELECT * FROM user;
+-- SHOW GRANTS FOR 'username'@'localhost'
+-- SHOW GRANTS;
+-- DROP USER 'username'@'localhost';
