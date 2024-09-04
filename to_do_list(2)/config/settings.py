29b5d26a64d10c50d0745a4e4a1505ef2a2cae93
leta <hyeok8366@gmail.com>
2024-09-04 17:19:13 +0900

    장고 2차 과제제출

--- to_do_list(2)/config/settings.py
+++ to_do_list(2)/config/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todo',
-    'users',
 ]
 
 MIDDLEWARE = [
@@ -118,9 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-LOGIN_REDIRECT_URL = '/todo/'
-LOGOUT_REDIRECT_URL = '/todo/'
-LOGIN_URL = '/accounts/login/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
